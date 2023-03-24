5dd6ee272aaf6d932b72bd6c6345f065eeb52918
Grant J. Butler <grantjbutler@users.noreply.github.com>
2023-03-24 13:27:29 -0400

    Attempt to fix error handling.

--- index.js
+++ index.js
@@ -8,60 +8,72 @@ let client = new TiltifyClient(process.env.TILTIFY_ACCESS_TOKEN)
 let interval = process.env.REFRESH_INTERVAL || 5000
 
 let lastSeenDonationID = process.env.LAST_SEEN_DONATION_ID || ''
-function getRecentDonations() {
-    client.Campaigns.getRecentDonations(process.env.CAMPAIGN_ID, (donations) => {
-        for (let donation of donations) {
-            if (donation.id == lastSeenDonationID) {
-                break
+async function getRecentDonations() {
+    try {
+        client.Campaigns.getRecentDonations(process.env.CAMPAIGN_ID, (donations) => {
+            for (let donation of donations) {
+                if (donation.id == lastSeenDonationID) {
+                    break
+                }
+
+                let overlayDonation = {
+                    id: String(donation.id),
+                    name: donation.name,
+                    amount: donation.amount,
+                    date_created: new Date(donation.completedAt)
+                }
+
+                if (donation.comment != null) {
+                    overlayDonation.comment = donation.comment
+                }
+
+                io.emit('donation', overlayDonation)
+                console.log(`> Emitting donation: ${util.inspect(overlayDonation)}`)
+            }
+            
+            if (donations.length > 0) {
+                lastSeenDonationID = String(donations[0].id)
             }
 
-            let overlayDonation = {
-                id: String(donation.id),
-                name: donation.name,
-                amount: donation.amount,
-                date_created: new Date(donation.completedAt)
-            }
-
-            if (donation.comment != null) {
-                overlayDonation.comment = donation.comment
-            }
-
-            io.emit('donation', overlayDonation)
-            console.log(`> Emitting donation: ${util.inspect(overlayDonation)}`)
-        }
-        
-        if (donations.length > 0) {
-            lastSeenDonationID = String(donations[0].id)
-        }
-
+            setTimeout(() => {
+                getRecentDonations()
+            }, interval)
+        })
+    } catch (error) {
         setTimeout(() => {
             getRecentDonations()
         }, interval)
-    })
+    }
 }
 
 let campaignTotal = 0.00
 let campaignTarget = 0.00
-function getTotal() {
-    client.Campaigns.get(process.env.CAMPAIGN_ID, (campaign) => {
-        if (campaignTotal != campaign.amountRaised) {
-            campaignTotal = campaign.amountRaised
-            
-            io.emit('total', campaignTotal)
-            console.log(`> Emitting total: ${campaignTotal}`)
-        }
+async function getTotal() {
+    try {
+        await client.Campaigns.get(process.env.CAMPAIGN_ID, (campaign) => {
+            if (campaignTotal != campaign.amountRaised) {
+                campaignTotal = campaign.amountRaised
+                
+                io.emit('total', campaignTotal)
+                console.log(`> Emitting total: ${campaignTotal}`)
+            }
 
-        if (campaignTarget != campaign.fundraiserGoalAmount) {
-            campaignTarget = campaign.fundraiserGoalAmount
+            if (campaignTarget != campaign.fundraiserGoalAmount) {
+                campaignTarget = campaign.fundraiserGoalAmount
 
-            io.emit('target', campaignTarget);
-            console.log(`> Emitting total: ${campaignTarget}`)
-        }
+                io.emit('target', campaignTarget);
+                console.log(`> Emitting total: ${campaignTarget}`)
+            }
 
+            setTimeout(() => {
+                getTotal()
+            }, interval)
+        })
+    } catch (error) {
         setTimeout(() => {
             getTotal()
         }, interval)
-    })
+    }
 }
 
 let port = process.env.PORT || 8081
